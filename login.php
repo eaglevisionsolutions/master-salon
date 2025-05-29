1489b251e9752461d81721d81129dc9d3e6c12d7
eaglevisionsolutions <info@eaglevisionsolutions.ca>
2025-05-29 13:27:27 -0400

    fixed up form formatting for login

--- login.php
+++ login.php
@@ -9,14 +9,17 @@ require_once 'templates/header.php';
 $csrf_token = generate_csrf_token();
 ?>
 <h2>Login</h2>
-<form id="loginForm">
-    <div id="loginError"></div> <!-- Error message will be injected here -->
-    <input type="text" id="loginUsername" name="username" placeholder="Username" required><br>
-    <input type="password" id="loginPassword" name="password" placeholder="Password" required><br>
-    <input type="hidden" id="csrf_token" name="csrf_token" value="<?= $csrf_token ?>">
-    <button type="submit">Login</button>
-</form>
-<p>Don't have an account? <a href="register.php">Register</a></p>
+<div class="formContainer">
+    <p>Please enter your username and password to log in.</p>
+    <form id="loginForm">
+        <div id="loginError"></div> <!-- Error message will be injected here -->
+        <input type="text" id="loginUsername" name="username" placeholder="Username" required><br>
+        <input type="password" id="loginPassword" name="password" placeholder="Password" required><br>
+        <input type="hidden" id="csrf_token" name="csrf_token" value="<?= $csrf_token ?>">
+        <button type="submit">Login</button>
+    </form>
+    <p>Don't have an account? <a href="register.php">Register</a></p>
+</div>
 <script>
 window.CSRF_TOKEN = "<?= $csrf_token ?>";
 </script>
